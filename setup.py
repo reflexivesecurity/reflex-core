9684666411bc54ef1e8c1b649ba474e4d5d39578
Max Anderson <maxwell.c.anderson@gmail.com>
2020-01-15 15:39:30 -0800

    Fix license classifier

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MPL2 License",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
