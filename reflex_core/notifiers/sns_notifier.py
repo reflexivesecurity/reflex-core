14e09a1aee1f9346f67ed2812392c5a571c6b975
Max Anderson <maxwell.c.anderson@gmail.com>
2020-06-14 20:46:15 -0700

    Switch to Google style docstrings

--- reflex_core/notifiers/sns_notifier.py
+++ reflex_core/notifiers/sns_notifier.py
@@ -12,11 +12,20 @@ class SNSNotifier(Notifier):
     CLIENT = boto3.client("sns")
 
     def notify(self, subject, message):
-        """ Sends a notification message via SNS. """
+        """Sends a notification message via SNS.
+
+        Args:
+            subject (str): The notification subject.
+            message (str): The notification message.
+        """
         sns_topic = self.get_sns_topic()
 
         self.CLIENT.publish(TopicArn=sns_topic, Subject=subject, Message=message)
 
     def get_sns_topic(self):
-        """ Get the SNS topic to notify. """
+        """Get the SNS topic to send a notification to.
+
+        Returns:
+            str: The SNS topic ARN which we will send a message to.
+        """
         return os.environ["SNS_TOPIC"]
